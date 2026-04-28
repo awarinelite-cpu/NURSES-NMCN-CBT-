0e8cc4623dd8085cfa9ae1f01c7fb4b109f6bcc9
awarinelite-cpu <awarinelite@gmail.com>
2026-04-28 22:36:33 +0100

    Update EntranceResultsPages.jsx

--- Nurses-Academy-CBT-main/src/components/entrance/EntranceResultsPages.jsx
+++ Nurses-Academy-CBT-main/src/components/entrance/EntranceResultsPages.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { db } from '../../firebase';
+import { db } from '../../firebase/config';;
 import {
   collection, doc, getDoc, getDocs, query, orderBy,
   limit, deleteDoc, where, Timestamp
